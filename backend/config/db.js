ebb1094187db8c3d9844bf11355c2ef4c265b234
RohanJung <rohanjungtmg@gmail.com>
2024-08-21 22:33:34 +0545

    added model controller asyncHandler route

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,13 @@ import mongoose from "mongoose";
 
 const connectDb =  async()  => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/test");
+        await mongoose.connect('mongodb://127.0.0.1:27017/test');
         console.log('sucessfully connected');
     }
-    catch(eror){
+    catch(eror){        
         console.log(`ERROR: ${eror.message}`);
         process.exit(0);
-
     }
 }
-
-export default connectDb;
+export default connectDb;       
+                    
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';   
 import connectDb from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,8 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.send('hello world');
-})
+app.get('/api/uesrs',userRoutes);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
