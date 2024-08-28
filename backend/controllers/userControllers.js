fd67b37d24b3dacf0e39c2c1210ba0e38fb423c2
RohanJung <rohanjungtmg@gmail.com>
2024-08-28 21:06:51 +0545

    created user account creation and database insertion ,fixed env file and async Handler

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,8 +1,42 @@
-import User from '../models/userModels.js'; 
-import asyncHandler from '../middlewares/asyncHandler.js';
+import User from "../models/userModels.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrpyt from "bcryptjs";
 
-const createUser = asyncHandler(async(req,res)=>{
-    res.send("hello");
+
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if(!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all fields.");
+  };
+
+  const userExists = await User.findOne({email});
+
+  
+  if(userExists)  {  res.status(201).send("User already exits") };
+  console.log(username, email, password);
+
+  const salt = await bcrpyt.genSalt(10);
+  const hashedPassword = await bcrpyt.hash(password, salt); 
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+
+    res
+      .status(201)
+      .json({
+        _id: newUser._id,
+        username: newUser.username,
+    email: newUser.email,
+        password: newUser.password,
+      });
+  } catch(error) {
+    res.status(400);
+    throw new Error("An error occurred. Please try again.");
+  }
 });
 
-export {createUser};
+export { createUser };
--- backend/middlewares/asyncHandler.js
+++ backend/middlewares/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler =  (fn )=> (req,res,next) => {
     Promise.resolve(fn(req,res,next)).catch((error)=>{
-        res.status(500).josn({mesage: error.message});
+        res.status(500).json({mesage: error.message});
     });
 };
 
