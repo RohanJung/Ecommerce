a955ee1e0ebca9e6621f577e7fd98d9bbb4782cf
RohanJung <rohanjungtmg@gmail.com>
2024-08-30 20:53:53 +0545

    implemented new route for checking if a user exists or not

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,7 @@ import User from "../models/userModels.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrpyt from "bcryptjs";
 import createToken from "../utils/createToken.js";
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 
 
 const createUser = asyncHandler(async (req, res) => {
@@ -29,7 +30,7 @@ const createUser = asyncHandler(async (req, res) => {
     console.log('new user created');
 
     res
-      .status(201)
+      .status(200)
       .json({
         _id: newUser._id,
         username: newUser.username,
@@ -42,4 +43,34 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isValidPassword = await bcrypt.compare(password, existingUser.password);
+    if (isValidPassword) {
+      console.log('You exist');
+      createToken(res, existingUser._id);
+      res.status(200).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        password: existingUser.password
+      });
+    } else {
+      res.status(401).json({
+        error: "unauthorized",
+        message: "Invalid username or password"
+      });
+    }
+  }
+else{
+  res.status(401).json({
+    error:"unathorized",
+    "message":"The user doesnt exist"
+  })
+}
+});
+
+export { createUser,loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { createUser } from "../controllers/userControllers.js";
-
+import { createUser,loginUser } from "../controllers/userControllers.js";
 const  router = express.Router();
 
 router.post('/',createUser);
+router.get('/auth',loginUser);
 
 export default router;
