bbb56685b45484ab574d6101c40dac56ec1297f5
RohanJung <rohanjungtmg@gmail.com>
2024-09-02 13:45:32 +0545

    Implemented user deletion from by the admin

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,42 +3,39 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrpyt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 import bcrypt from "bcryptjs/dist/bcrypt.js";
-import express from "express";
-
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
-  if(!username || !email || !password) {
+  if (!username || !email || !password) {
     res.status(400);
     throw new Error("Please fill in all fields.");
-  };
+  }
 
-  const userExists = await User.findOne({email});
+  const userExists = await User.findOne({ email });
 
-  
-  if(userExists)  {  res.status(201).send("User already exits") };
+  if (userExists) {
+    res.status(201).send("User already exits");
+  }
   console.log(username, email, password);
 
   const salt = await bcrpyt.genSalt(10);
-  const hashedPassword = await bcrpyt.hash(password, salt); 
+  const hashedPassword = await bcrpyt.hash(password, salt);
 
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
-    createToken(res,newUser._id);
-    console.log('new user created');
+    createToken(res, newUser._id);
+    console.log("new user created");
 
-    res
-      .status(200)
-      .json({
-        _id: newUser._id,
-        username: newUser.username,
-    email: newUser.email,
-        password: newUser.password,
-      });
-  } catch(error) {
+    res.status(200).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      password: newUser.password,
+    });
+  } catch (error) {
     res.status(400);
     throw new Error("An error occurred. Please try again.");
   }
@@ -49,95 +46,118 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    const isValidPassword = await bcrypt.compare(password, existingUser.password);
+    const isValidPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
     if (isValidPassword) {
-      console.log('You exist');
+      console.log("You exist");
       createToken(res, existingUser._id);
       res.status(200).json({
         _id: existingUser._id,
         username: existingUser.username,
         email: existingUser.email,
-        password: existingUser.password
+        password: existingUser.password,
       });
     } else {
       res.status(401).json({
         error: "unauthorized",
-        message: "Invalid username or password"
+        message: "Invalid username or password",
       });
     }
+  } else {
+    res.status(401).json({
+      error: "unathorized",
+      message: "The user doesnt exist",
+    });
   }
-else{
-  res.status(401).json({
-    error:"unathorized",
-    "message":"The user doesnt exist"
-  })
-}
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
   if (!req.cookies.jwt) {
-    return res.status(401).json({ "message": "User doesnt exist" });
+    return res.status(401).json({ message: "User doesnt exist" });
   }
-  res.cookie('jwt', '', {
+  res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
   });
   res.status(200).json({
-    "message": "User logout Sucesfully"
+    message: "User logout Sucesfully",
   });
-
 });
 
-const getAllUsers = asyncHandler(async(req,res) => {
+const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   console.log(users);
 
   res.json(users);
-})
-const getProfileDetail = asyncHandler(async(req,res)=>{
+});
+const getProfileDetail = asyncHandler(async (req, res) => {
   try {
     const userId = await User.findById(req.user._id);
-    
+
     if (userId) {
       res.status(200).json({
-        "_id": userId._id,
-        "username": userId.username,
-        "email": userId.email,
-        "password":userId.password,
+        _id: userId._id,
+        username: userId.username,
+        email: userId.email,
+        password: userId.password,
       });
     } else {
-      res.status(400).json({ "message": "The user info is not available" });
+      res.status(400).json({ message: "The user info is not available" });
     }
   } catch (error) {
-    console.error("Error fetching user:", error); 
-    res.status(500).json({ "message": "Internal server error" }); 
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-  
-})
-const updateProfileDetail = asyncHandler(async(req,res)=>{
-    const user = await User.findById(req.user._id);
+});
+const updateProfileDetail = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
-    if(user){
-      user.username = req.body.username;
-      user.email = req.body.email;
-      
-      if(req.body.password){
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password,salt);
-        user.password = hashedPassword;
-      }
-      const Updateduser = await user.save();
+  if (user) {
+    user.username = req.body.username;
+    user.email = req.body.email;
 
-      res.json({
-        _id:Updateduser._id,
-        username:Updateduser.username,
-        email:Updateduser.email,
-        isAdmin:Updateduser.isAdmin,
-      })
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
     }
-    else{
-      res.status(404);
-      throw new Error("User not found");
+    const Updateduser = await user.save();
+
+    res.json({
+      _id: Updateduser._id,
+      username: Updateduser.username,
+      email: Updateduser.email,
+      isAdmin: Updateduser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+const deleteUserbyId = asyncHandler(async (req, res) => {
+  const user = await  User.findById(req.params.id);
+  console.log(user);
+  if (user) {
+    if (user.isAdmin) {
+      throw new Error("Cannot delete a admin");
     }
-}) 
-export { createUser,loginUser,logoutUser,getAllUsers,getProfileDetail,updateProfileDetail};
+    await User.deleteOne({ _id: user._id });
+    res.json({
+      message: "User deleted",
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+export {
+  createUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+  getProfileDetail,
+  updateProfileDetail,
+  deleteUserbyId,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createUser,loginUser,logoutUser,getAllUsers,getProfileDetail,updateProfileDetail } from "../controllers/userControllers.js";
+import { createUser,loginUser,logoutUser,getAllUsers,getProfileDetail,updateProfileDetail,deleteUserbyId } from "../controllers/userControllers.js";
 import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
 const  router = express.Router();
 
 router.post('/',createUser);
 router.get('/',authenticate,authorizeAdmin,getAllUsers);
 router.post('/auth',loginUser);
-router.route('/profile').get(authenticate,getProfileDetail).put(authenticate,   updateProfileDetail);
+router.route('/profile').get(authenticate,getProfileDetail).put(authenticate,updateProfileDetail);
+router.route('/:id').delete(authenticate,authorizeAdmin,deleteUserbyId);
 router.get('/logout',logoutUser);
 export default router;
