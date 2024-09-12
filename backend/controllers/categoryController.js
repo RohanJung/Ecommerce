e3cc8209dc82757333e7a8d935d94d5d4a7e7155
RohanJung <rohanjungtmg@gmail.com>
2024-09-12 17:26:52 +0545

    delete and update category

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -27,4 +27,28 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
-export { createCategory };
+const updateCategory = asyncHandler(async (req, res) => {
+  const name = req.body.name;
+  const category = await Category.findOne(req.body.name);
+  console.log(category);
+  if (category) {
+    category.name = name;
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } else {
+    res.status(404).json({ message: "Category not found" });
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  const name = req.body.name;
+  const category = await Category.findOne(name);
+  if (category) {
+    await category.remove();
+    res.json({ message: "Category removed" });
+  } else {
+    res.status(404).json({ message: "Category not found" });
+  }
+});
+
+export { createCategory, updateCategory, deleteCategory };
