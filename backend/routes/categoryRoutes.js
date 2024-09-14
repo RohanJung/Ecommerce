3dc0d66701f19a1d4303aac237b5b32d9540b6d8
RohanJung <rohanjungtmg@gmail.com>
2024-09-14 11:46:54 +0545

    created route for categories crud

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categoryController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/", createCategory);
+router.post("/update", updateCategory);
+router.post("/delete", deleteCategory);
 export default router;
